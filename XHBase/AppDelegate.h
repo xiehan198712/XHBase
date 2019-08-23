568e06c04d0663d8bf46a62a71c36d7535e7a96a
chinkout@163.com <chinkout@163.com>
2019-08-23 23:30:33 +0800

    Initial Commit

---
+++ XHBase/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XHBase
+//
+//  Created by 谢涵 on 2019/8/23.
+//  Copyright © 2019 Hans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XHBase/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  XHBase
+//
+//  Created by 谢涵 on 2019/8/23.
+//  Copyright © 2019 Hans. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
